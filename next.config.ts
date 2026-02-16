8b8fec46562c32e5819a611442081fd6ef266d70
Asaf <asafa1@northsidemarine.com.au>
2026-02-16 23:47:20 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://www.yam

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.yamaha-motor.com.au',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   webpack: (config) => {
