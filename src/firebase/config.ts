885936fc28d8740efa1e603878fd215f08cf983f
Asaf <asafa1@northsidemarine.com.au>
2026-02-24 22:52:43 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,5 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyDJ7b5G9zL2uTpDzgwTLvQtVUIyaVBpSBY",
   "authDomain": "studio-2290360004-3b963.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "611154837797",
-  "storageBucket": "studio-2290360004-3b963.firebasestorage.app"
+  "messagingSenderId": "611154837797"
 };
