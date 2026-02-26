a56bf5ab6a8ed592a2439cb8751b7e402c67a029
Asaf <asafa1@northsidemarine.com.au>
2026-02-26 03:27:30 +0000

    Try fixing this error: `Runtime ReferenceError: Separator is not defined

--- src/app/(app)/data-warehouse/[id]/ranges/[rangeId]/page.tsx
+++ src/app/(app)/data-warehouse/[id]/ranges/[rangeId]/page.tsx
@@ -42,6 +42,7 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
 import { createSlug, cn } from '@/lib/utils';
 import Link from 'next/link';
 import { useUser } from '@/firebase/auth/use-user';
