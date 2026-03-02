8e68b8d134d8e1ef7881a9f4f6fff2b99485dc2d
Asaf <asafa1@northsidemarine.com.au>
2026-03-02 02:35:39 +0000

    ## Error Type Runtime ReferenceError
    ## Error Message
    Card is not defin

--- src/components/master-data-browser-dialog.tsx
+++ src/components/master-data-browser-dialog.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useState, useMemo, useEffect } from 'react';
@@ -14,6 +13,7 @@ import {
 } from '@/components/ui/dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { Loader2, PackagePlus, X, Plus, Table as TableIcon, Search, PlusCircle, CheckCircle2, AlertCircle } from 'lucide-react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Input } from '@/components/ui/input';
